a9aad6c2a66c664196f6ad09abae2c9a6101209e
daoket <daoket@163.com>
2018-03-04 14:29:43 +0800

    :zap:

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # parcel-plugin-zip
+目的：压缩构建后的文件夹
+
+## 安装
+
+```
+npm install parcel-plugin-zip -D
+```
+
+## 构建
+
+```
+npm run build
+```
+
+## 输出
+
+Output a `[projectName].zip` file.
+
+License
+========
+
+MIT
+
+
+
--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-var fs = require('fs');
-var archiver = require('archiver');
+const fs = require('fs')
+const path = require('path')
+const archiver = require('archiver')
 
+module.exports = bundler => {
 
-var output = fs.createWriteStream('dist.zip');
-var archive = archiver('zip');
+  bundler.on('bundled', async (bundle) => {
+    if (process.env.NODE_ENV === 'production') {
+      const dir = path.dirname(bundle.name)
 
-archive.on('error', function (err) {
-	throw err;
-});
+      var output = fs.createWriteStream(path.parse(dir).name + '.zip')
+      var archive = archiver('zip')
 
-archive.pipe(output);
-archive.directory('dist/')
-archive.finalize();
+      archive.on('error', err => {throw err})
+
+      archive.pipe(output);
+      archive.directory(path.parse(dir).name, false)
+      archive.finalize()
+
+      function removeFile() {
+        return new Promise((resolve, reject) => {
+          fs.readdir(dir, (err, files) => {
+            files.forEach(file => {
+              fs.unlink(path.join(dir, file), err => {console.log(err)})
+              resolve(6)
+            })
+          })
+        })
+      }
+
+      async function removeDir() {
+        await removeFile()
+        fs.rmdir(dir, function (err) {
+          if (err) return console.error(err)
+          console.log("success!")
+        })
+      }
+      removeDir()
+    }
+  })
+}
