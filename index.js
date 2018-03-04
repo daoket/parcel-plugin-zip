42290a1eaad40163d5260320af90f943a1328822
daoket <daoket@163.com>
2018-03-04 17:36:33 +0800

    :checkedred_falg:

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+var fsextra = require('fs-extra')
 const archiver = require('archiver')
 
 module.exports = bundler => {
@@ -8,29 +9,24 @@ module.exports = bundler => {
     if (process.env.NODE_ENV === 'production') {
       const dir = path.dirname(bundle.name)
 
-      var output = fs.createWriteStream(path.parse(dir).name + '.zip')
-      var archive = archiver('zip')
+      function zipFile() {
+        return new Promise(resolve => {
+          var output = fs.createWriteStream(path.parse(dir).name + '.zip')
+          var archive = archiver('zip')
 
-      archive.on('error', err => {throw err})
-
-      archive.pipe(output);
-      archive.directory(path.parse(dir).name, false)
-      archive.finalize()
-
-      function removeFile() {
-        return new Promise((resolve, reject) => {
-          fs.readdir(dir, (err, files) => {
-            files.forEach(file => {
-              fs.unlink(path.join(dir, file), err => {console.log(err)})
-              resolve(6)
-            })
+          archive.on('error', err => { throw err })
+          output.on('close', function () {
+            resolve('Data has been close')
           })
+
+          archive.pipe(output);
+          archive.directory(path.parse(dir).name, false)
+          archive.finalize()
         })
       }
-
       async function removeDir() {
-        await removeFile()
-        fs.rmdir(dir, function (err) {
+        await zipFile()
+        fsextra.remove(dir, function (err) {
           if (err) return console.error(err)
           console.log("success!")
         })
